0633ed5106356b5c19240147f391c05fbd7eeb01
ThomasLeMah <129088785+ThomasLeMah@users.noreply.github.com>
2023-03-28 12:22:49 +0200

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -4,7 +4,7 @@ export const getGlobalData = () => {
     : 'Thomas Lafforgue';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'Monteur Vidéo';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : '';
